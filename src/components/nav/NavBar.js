cca281c084fad41df8e67dfe5fd1558bdcc0dba7
Yelena Cox <yelenacox@gmail.com>
2022-10-13 22:13:18 -0500

    added products and locations

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -3,15 +3,25 @@ import "./NavBar.css"
 
 export const NavBar = () => {
     const navigate = useNavigate()
-
+    const user = JSON.parse(localStorage.getItem("kandy_user"))
     return (
         <ul className="navbar">
-            <li className="navbar__item navbar__logout">
-                <Link className="navbar__link" to="" onClick={() => {
-                    localStorage.removeItem("kandy_user")
-                    navigate("/", {replace: true})
-                }}>Logout</Link>
+            <li className="navbar__item active">
+                <Link className="navbar__link" to="/locations">Locations</Link>
             </li>
+            {user.staff ?
+            <li className="navbar__item active">
+                <Link className="navbar__link" to="/products">Products</Link>
+            </li>         
+            : ''}
+            {
+                <li className="navbar__item navbar__logout">
+                    <Link className="navbar__link" to="" onClick={() => {
+                        localStorage.removeItem("kandy_user")
+                        navigate("/", { replace: true })
+                    }}>Logout</Link>
+                </li>
+                }
         </ul>
     )
 }
--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,6 +1,22 @@
-export const ApplicationViews = () => {
-	return <>
+import { Outlet, Route, Routes } from "react-router-dom"
+import { LocationList } from "../locations/locations"
+import { ProductList } from "../products/products"
 
-	</>
+export const ApplicationViews = () => {
+	return (
+		<Routes>
+			<Route path="/" element={
+				<>
+				<h1>Kandy Korner</h1>
+				<Outlet />
+				</>
+			}>
+				<Route path="locations" element={ <LocationList />} />
+				<Route path="products" element={<ProductList />} />
+			</Route>
+		</Routes>
+	)
+	
+
 }
 
